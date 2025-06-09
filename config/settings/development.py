3ba9d0a871b01a36506be03aa7cba693c0ff93cf
Lucas Zanon Guarnier <lucas.guarnier@globalsys.com.br>
2025-06-09 16:28:40 -0300

    erro: configurando origens confiaveis ao csrf

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,6 +7,13 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mixmaster-09dz.onrender.com']
 CORS_ALLOW_ALL_ORIGINS = True  # Only for development
 CORS_ALLOW_CREDENTIALS = True
 
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    'https://mixmaster-09dz.onrender.com',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
+
 # MongoDB settings (connection will be established when needed)
 MONGODB_URI = os.environ.get('MONGODB_URI')
 MONGODB_NAME = os.environ.get('MONGODB_NAME')
